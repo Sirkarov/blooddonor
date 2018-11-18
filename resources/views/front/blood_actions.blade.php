52f4592208719fa66976151370a1d9ff94c792c9
Sirkarov <josif_sirkarov@hotmail.com>
2018-11-18 19:06:32 +0100

    backgrodun color on card

--- resources/views/front/blood_actions.blade.php
+++ resources/views/front/blood_actions.blade.php
@@ -5,7 +5,7 @@
         <div class="col-lg-12 text-center" style="margin-bottom:10px"><h1><strong>Крводарителски Акции во Македонија 2018/19 година</strong></h1></div>
     @foreach($blood_actions as $action)
         <div class="col-lg-12" style="margin-bottom: 10px;">
-        <div class="card">
+        <div class="card" style=" background-color: lightgrey">
             <h5 class="card-header h5" style="background-color: #ff0000;color:white">{{$action->name}}</h5>
             <div class="card-body">
                 <div class="row text-center">
